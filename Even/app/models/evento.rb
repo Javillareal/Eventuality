21633339f9f5712f4233f2aef838f4cea315289d
Javillareal <jorgemil95@hotmail.com>
2016-11-15 18:15:39 -0500

    first commit

---
+++ Even/app/models/evento.rb
@@ -0,0 +1,5 @@
+class Evento < ActiveRecord::Base
+
+	geocoded_by :address
+	after_validation :geocode
+end
---
+++ Even/app/models/usuario.rb
@@ -0,0 +1,5 @@
+class Usuario < ActiveRecord::Base
+  has_secure_password
+  
+  validates_uniqueness_of :email
+end
